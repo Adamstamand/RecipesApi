9ec9ab7a3fbe2819c34b326ff5d5a366059ec163
Adamstamand <stamandadam@gmail.com>
2023-07-25 11:13:56 -0400

    Finished RecipeController tests

--- RecipesApi.Tests/Controllers/RecipeControllerTests.cs
+++ RecipesApi.Tests/Controllers/RecipeControllerTests.cs
@@ -156,4 +156,240 @@ public class RecipeControllerTests
 
         result.Result.Should().BeOfType<UnauthorizedObjectResult>();
     }
+
+    [Fact]
+    public async Task PostRecipe_ShouldReturnUnauthorized_WhenAuthorizationHeaderIsNotPresent()
+    {
+        var recipe = _fixture.Create<Recipe>();
+    _recipeController.HttpContext.Request.Headers.Remove("Authorization");
+
+        var result = await _recipeController.PostRecipe(recipe);
+
+        result.Should().BeOfType<UnauthorizedObjectResult>();
+    }
+    
+    [Fact]
+    public async Task PostRecipe_ShouldReturnBadRequest_WhenRecipeNameAlreadyExists()
+    {
+        var recipe = _fixture.Create<Recipe>();
+        var user = _fixture.Create<ApplicationUser>();
+        _userRepositoryMock
+            .Setup(m => m.FindUserFromJwtHeader(It.IsAny<StringValues>()))
+            .ReturnsAsync(user);
+        _recipesRepositoryMock
+            .Setup(m => m.DoesRecipeExistAlready(recipe))
+            .ReturnsAsync(true);
+
+        var result = await _recipeController.PostRecipe(recipe);
+
+        result.Should().BeOfType<BadRequestObjectResult>();
+    }
+
+    [Fact]
+    public async Task PostRecipe_ShouldReturnCreatedAtAction_OnSuccess()
+    {
+        var recipe = _fixture.Create<Recipe>();
+        var user = _fixture.Create<ApplicationUser>();
+        _userRepositoryMock
+            .Setup(m => m.FindUserFromJwtHeader(It.IsAny<StringValues>()))
+            .ReturnsAsync(user);
+        _recipesRepositoryMock
+            .Setup(m => m.DoesRecipeExistAlready(It.IsAny<Recipe>()))
+            .ReturnsAsync(false);
+        _recipesRepositoryMock
+            .Setup(m => m.AddRecipe(It.IsAny<UserRecipe>()))
+            .ReturnsAsync(recipe);
+
+        var result = await _recipeController.PostRecipe(recipe);
+
+        result.Should().BeOfType<CreatedAtActionResult>();
+    }
+
+    [Fact]
+    public async Task DeleteRecipe_ShouldReturnUnauthorized_WhenAuthorizationHeaderIsNotPresent()
+    {     
+        _recipeController.HttpContext.Request.Headers.Remove("Authorization");
+
+        var result = await _recipeController.DeleteRecipe(It.IsAny<int>());
+
+        result.Should().BeOfType<UnauthorizedObjectResult>();
+    }
+
+    [Fact]
+    public async Task DeleteRecipe_ShouldReturnUnauthorized_WhenUserDoesNotExist()
+    {
+        _userRepositoryMock
+            .Setup(m => m.FindUserFromJwtHeader(It.IsAny<StringValues>()))
+            .ReturnsAsync(default(ApplicationUser?));
+
+        var result = await _recipeController.DeleteRecipe(It.IsAny<int>());
+
+        result.Should().BeOfType<UnauthorizedObjectResult>();
+    }
+    
+    [Fact]
+    public async Task DeleteRecipe_ShouldReturnUnauthorized_WhenUserDidNotCreateRecipe()
+    {
+        var user = _fixture.Create<ApplicationUser>();
+        _userRepositoryMock
+            .Setup(m => m.FindUserFromJwtHeader(It.IsAny<StringValues>()))
+            .ReturnsAsync(user);
+        _recipesRepositoryMock
+            .Setup(m => m.CheckRecipeAccess(It.IsAny<int>(), It.IsAny<ApplicationUser>()))
+            .ReturnsAsync(false);
+
+        var result = await _recipeController.DeleteRecipe(It.IsAny<int>());
+
+        result.Should().BeOfType<UnauthorizedObjectResult>();
+    }
+    
+    [Fact]
+    public async Task DeleteRecipe_ShouldReturnNoContent_WhenRecipeIsDeleted()
+    {
+        var user = _fixture.Create<ApplicationUser>();
+        _userRepositoryMock
+            .Setup(m => m.FindUserFromJwtHeader(It.IsAny<StringValues>()))
+            .ReturnsAsync(user);
+        _recipesRepositoryMock
+            .Setup(m => m.CheckRecipeAccess(It.IsAny<int>(), It.IsAny<ApplicationUser>()))
+            .ReturnsAsync(true);
+        _recipesRepositoryMock
+            .Setup(m => m.RemoveRecipe(It.IsAny<int>()))
+            .ReturnsAsync("deleted");
+
+        var result = await _recipeController.DeleteRecipe(It.IsAny<int>());
+
+        result.Should().BeOfType<NoContentResult>();
+    }
+    
+    [Fact]
+    public async Task DeleteRecipe_ShouldReturnBadRequest_WhenRecipeFailedToDelete()
+    {
+        var user = _fixture.Create<ApplicationUser>();
+        _userRepositoryMock
+            .Setup(m => m.FindUserFromJwtHeader(It.IsAny<StringValues>()))
+            .ReturnsAsync(user);
+        _recipesRepositoryMock
+            .Setup(m => m.CheckRecipeAccess(It.IsAny<int>(), It.IsAny<ApplicationUser>()))
+            .ReturnsAsync(true);
+        _recipesRepositoryMock
+            .Setup(m => m.RemoveRecipe(It.IsAny<int>()))
+            .ReturnsAsync(default(string?));
+
+        var result = await _recipeController.DeleteRecipe(It.IsAny<int>());
+
+        result.Should().BeOfType<BadRequestObjectResult>();
+    }
+
+    [Fact]
+    public async Task PutRecipe_ShouldReturnBadRequest_IfIdsDoNotMatch()
+    {
+        int id = 1;
+        var recipe = _fixture
+            .Build<Recipe>()
+            .With(m => m.Id, 2)
+            .Create();
+
+        var result = await _recipeController.PutRecipe(id, recipe);
+
+        result.Should().BeOfType<BadRequestObjectResult>();
+    }
+
+    [Fact]
+    public async Task PutRecipe_ShouldReturnUnauthorized_WhenAuthorizationHeaderIsNotPresent()
+    {
+        int id = 2;
+        var recipe = _fixture
+            .Build<Recipe>()
+            .With(m => m.Id, 2)
+            .Create();
+        _recipeController.HttpContext.Request.Headers.Remove("Authorization");
+
+        var result = await _recipeController.PutRecipe(id, recipe);
+
+        result.Should().BeOfType<UnauthorizedObjectResult>();
+    }
+    
+    [Fact]
+    public async Task PutRecipe_ShouldReturnUnauthorized_WhenUserDoesNotExist()
+    {
+        int id = 2;
+        var recipe = _fixture
+            .Build<Recipe>()
+            .With(m => m.Id, 2)
+            .Create();
+        _userRepositoryMock
+            .Setup(m => m.FindUserFromJwtHeader(It.IsAny<StringValues>()))
+            .ReturnsAsync(default(ApplicationUser?));
+
+        var result = await _recipeController.PutRecipe(id, recipe);
+
+        result.Should().BeOfType<UnauthorizedObjectResult>();
+    }
+    
+    [Fact]
+    public async Task PutRecipe_ShouldReturnUnauthorized_WhenUserDidNotCreateTheRecipe()
+    {
+        int id = 2;
+        var recipe = _fixture
+            .Build<Recipe>()
+            .With(m => m.Id, 2)
+            .Create();
+        var user = _fixture.Create<ApplicationUser>();
+        _userRepositoryMock
+            .Setup(m => m.FindUserFromJwtHeader(It.IsAny<StringValues>()))
+            .ReturnsAsync(user);
+        _recipesRepositoryMock.Setup(m => m.CheckRecipeAccess(It.IsAny<int>(), It.IsAny<ApplicationUser>()))
+            .ReturnsAsync(false);
+
+        var result = await _recipeController.PutRecipe(id, recipe);
+
+        result.Should().BeOfType<UnauthorizedObjectResult>();
+    }
+    
+    [Fact]
+    public async Task PutRecipe_ShouldReturnNoContent_OnSuccess()
+    {
+        int id = 2;
+        var recipe = _fixture
+            .Build<Recipe>()
+            .With(m => m.Id, 2)
+            .Create();
+        var user = _fixture.Create<ApplicationUser>();
+        _userRepositoryMock
+            .Setup(m => m.FindUserFromJwtHeader(It.IsAny<StringValues>()))
+            .ReturnsAsync(user);
+        _recipesRepositoryMock.Setup(m => m.CheckRecipeAccess(It.IsAny<int>(), It.IsAny<ApplicationUser>()))
+            .ReturnsAsync(true);
+        _recipesRepositoryMock
+            .Setup(m => m.UpdateRecipe(It.IsAny<Recipe>()))
+            .ReturnsAsync("updated");
+
+        var result = await _recipeController.PutRecipe(id, recipe);
+
+        result.Should().BeOfType<NoContentResult>();
+    }
+    
+    [Fact]
+    public async Task PutRecipe_ShouldReturnBadRequest_WhenUpdateRecipeFails()
+    {
+        int id = 2;
+        var recipe = _fixture
+            .Build<Recipe>()
+            .With(m => m.Id, 2)
+            .Create();
+        var user = _fixture.Create<ApplicationUser>();
+        _userRepositoryMock
+            .Setup(m => m.FindUserFromJwtHeader(It.IsAny<StringValues>()))
+            .ReturnsAsync(user);
+        _recipesRepositoryMock.Setup(m => m.CheckRecipeAccess(It.IsAny<int>(), It.IsAny<ApplicationUser>()))
+            .ReturnsAsync(true);
+        _recipesRepositoryMock
+            .Setup(m => m.UpdateRecipe(It.IsAny<Recipe>()))
+            .ReturnsAsync(default(string?));
+
+        var result = await _recipeController.PutRecipe(id, recipe);
+
+        result.Should().BeOfType<BadRequestResult>();
+    }
 }
